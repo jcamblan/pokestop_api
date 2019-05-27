9277323518521ddf03be76ad7231c370aa670e82
Julien Camblan <julien@tymate.com>
2019-05-27 08:39:14 +0200

    chore: update pokemon & move migrations

--- db/migrate/20190526092749_create_pokemons.rb
+++ db/migrate/20190526092749_create_pokemons.rb
@@ -1,8 +1,28 @@
-class CreatePokemons < ActiveRecord::Migration[6.0]
-  def change
-    create_table :pokemons do |t|
+# frozen_string_literal: true
 
+# Pokemons
+class CreatePokemons < ActiveRecord::Migration[6.0]
+  def change # rubocop:disable Metrics/MethodLength
+    create_table :pokemons do |t|
+      t.string :num
+      t.bigint :generation_id
+      t.integer :candy_id
+      t.integer :candy_distance
+      t.text :pokedex_entry
+      t.text :comment
+      t.decimal :flee_rate
+      t.decimal :capture_rate
       t.timestamps
+    end
+
+    reversible do |dir|
+      dir.up do
+        Pokemon.create_translation_table! name: :string, description: :text
+      end
+
+      dir.down do
+        Pokemon.drop_translation_table!
+      end
     end
   end
 end
--- db/migrate/20190526092913_create_moves.rb
+++ db/migrate/20190526092913_create_moves.rb
@@ -1,8 +1,27 @@
+# frozen_string_literal: true
+
+# Moves
 class CreateMoves < ActiveRecord::Migration[6.0]
-  def change
+  def change # rubocop:disable Metrics/MethodLength
     create_table :moves do |t|
+      t.string :kind
+      t.integer :type_id
+      t.decimal :power
+      t.decimal :epu
+      t.decimal :cast_time
+      t.integer :energy_bars
 
       t.timestamps
+    end
+
+    reversible do |dir|
+      dir.up do
+        Move.create_translation_table! name: :string, description: :text
+      end
+
+      dir.down do
+        Move.drop_translation_table!
+      end
     end
   end
 end
