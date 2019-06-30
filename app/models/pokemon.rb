69038b0943485eaa2d21ce3b93d61087e9998232
Julien Camblan <julien.camblan@gmail.com>
2019-06-30 13:27:57 +0200

    feat: add methods to calculate pokemons cp/hp

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -8,11 +8,14 @@
 #  nid            :string           not null
 #  generation_id  :bigint           not null
 #  candy_id       :integer
-#  candy_distance :integer
+#  buddy_distance :integer
 #  kind           :string           default("normal")
 #  alolan_form_id :bigint
 #  flee_rate      :decimal(, )
 #  capture_rate   :decimal(, )
+#  base_atk       :integer
+#  base_def       :integer
+#  base_sta       :integer
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
 #
@@ -44,4 +47,26 @@ class Pokemon < ApplicationRecord
   # == Callbacks ===============================================================
   # == Class Methods ===========================================================
   # == Instance Methods ========================================================
+
+  def max_cp(level)
+    cp(15, 15, 15, level)
+  end
+
+  def min_cp(level)
+    cp(0, 0, 0, level)
+  end
+
+  def cp(ind_atk, ind_def, ind_sta, level)
+    attack = base_atk + ind_atk
+    defense = base_def + ind_def
+    stamina = base_sta + ind_sta
+    cpm = Utils::CPM_BY_LVL.dig(level)
+    cp = attack * defense**0.5 * stamina**0.5 * cpm**2 / 10
+    cp.to_i
+  end
+
+  def hp(level, ind_sta)
+    hp = Utils::CPM_BY_LVL.dig(level) * (base_sta + ind_sta)
+    hp.to_i
+  end
 end
