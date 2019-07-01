ef48fb3e692e6f21b3b020017dd658ee9fa004cd
Julien Camblan <julien@tymate.com>
2019-07-01 14:19:06 +0200

    chore: rubocop

--- app/graphql/types/generation_type.rb
+++ app/graphql/types/generation_type.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Types
   class GenerationType < Types::BaseObject
     field :id, ID, null: false
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
+  mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql' if Rails.env.development?
   post '/graphql', to: 'graphql#execute'
 end
